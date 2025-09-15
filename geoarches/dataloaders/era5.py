Updated feature branch with minor changes in the dataloader, added normalization of the forcings to the dataloader Fixed merge conflicts

geoarches/dataloaders/era5.py
@@ -9,6 +9,8 @@ import torch
 import xarray as xr
 from hydra.utils import instantiate
 
+import warnings
+
 from .era5_constants import (
     arches_default_level_variables,
     arches_default_pressure_levels,
@@ -120,7 +122,7 @@ class Era5Dataset(XarrayDataset):
             interpolate_nans=interpolate_nans,
         )
 
-    def convert_to_tensordict(self, xr_dataset):
+    def convert_to_tensordict(self, xr_dataset, debug=False):
         """
         input xarr should be a single time slice
         """
@@ -243,6 +245,7 @@ class Era5Forecast(Era5Dataset):
         stats_cfg,
         path: str = "data/era5_240/full/",
         forcings_path: str = None,
+        forcings_stats_path: str = None,
         domain: str = "train",
         filename_filter: Callable | None = None,
         timedelta_hours: int = None,
@@ -302,6 +305,29 @@ class Era5Forecast(Era5Dataset):
             raise ValueError(
                 "Either both forcings_path and forcing_vars must be set, or neither must be set."
             )
+        if forcings_path is not None:
+            self.forcings_ds = xr.open_dataset(forcings_path, **self.xr_options)
+            self.forcing_vars = list(forcing_vars)
+
+        if forcings_stats_path is not None:
+            self.forcings_stats_ds = xr.open_dataset(forcings_stats_path, **self.xr_options)
+            # check that all forcing vars are in stats ds
+            for var in self.forcing_vars:
+                if var not in self.forcings_stats_ds:
+                    raise ValueError(f"Forcing variable {var} not found in stats dataset.")
+            self.forcings_mean = torch.tensor(
+                self.forcings_stats_ds[self.forcing_vars].sel(statistic="mean").to_array().to_numpy()
+            ).float()
+            self.forcings_std = torch.tensor(
+                self.forcings_stats_ds[self.forcing_vars].sel(statistic="std").to_array().to_numpy()
+            ).float()
+        else:
+            warnings.warn(
+                "No forcings_stats_path provided. Forcings will not be normalized.", UserWarning
+            )
+            self.forcings_stats_ds = None
+            self.forcings_mean = None
+            self.forcings_std = None
         if forcings_path:
             print("##### FORCINGS: ", forcing_vars, " #####")
             self.forcings_ds = Era5Dataset(
@@ -430,6 +456,7 @@ class Era5Forecast(Era5Dataset):
                 i - self.lead_time_hours // self.timedelta,
                 interpolate_nans=self.interpolate_input,
                 warning_on_nan=self.warning_on_nan,
+                debug=False
             )
             out["prev_state"] = out["prev_state"]
 
@@ -458,8 +485,15 @@ class Era5Forecast(Era5Dataset):
             }
 
         first_day_of_month = timestamp.astype("datetime64[M]")
-        index = self.forcings_ds.timestamp_to_idx[str(first_day_of_month)]
-        return self.forcings_ds[index]["forcings"]
+        np_array = (
+            self.forcings_ds[self.forcing_vars].sel({self.time_dim_name: first_day_of_month}, method="nearest").to_array().to_numpy()
+        )
+        forcings = torch.from_numpy(np_array).float()
+
+        if self.forcings_stats_ds:
+            forcings = (forcings - self.forcings_mean[:, None, None]) / self.forcings_std[:, None, None]
+
+        return forcings
 
     def load_future_forcings(self, timestamp: np.datetime64):
         """